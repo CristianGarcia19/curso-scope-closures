65dde5d09b4a1e2d32ba0f5e2b8a3ba92d21d3f5
Cristian Garcia <cristian.gz1914@gmail.com>
2026-03-13 12:00:28 -0500

    Explicacion del scope de tipo block

---
+++ src/scope/block.js
@@ -0,0 +1,15 @@
+// Creacion de la funcion
+
+function frutas(){
+    if(true){ // Creacion del bloque de codigo
+        let fruta1 = 'Manzana';
+        let fruta2 = 'Pera';
+        const fruta3 = 'Kiwi';
+        console.log(fruta1);
+        console.log(fruta2);
+        console.log(fruta3);
+        // las variables se van a mostrar por que estan siendo llamadas dentro del bloque
+    }
+}
+
+frutas();
